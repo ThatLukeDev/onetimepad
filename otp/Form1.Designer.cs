58372faa0748bd3f3d3aaf98d4763970081fd87e
ThatLukeDev <luke@l2j.co.uk>
2024-06-05 18:11:55 +0100

    add

---
+++ otp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace otp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.content = new System.Windows.Forms.TextBox();
+            this.dimension = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.start = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // content
+            // 
+            this.content.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.content.Location = new System.Drawing.Point(0, 0);
+            this.content.Multiline = true;
+            this.content.Name = "content";
+            this.content.Size = new System.Drawing.Size(800, 450);
+            this.content.TabIndex = 0;
+            // 
+            // dimension
+            // 
+            this.dimension.Dock = System.Windows.Forms.DockStyle.Left;
+            this.dimension.Location = new System.Drawing.Point(0, 0);
+            this.dimension.Name = "dimension";
+            this.dimension.Size = new System.Drawing.Size(100, 20);
+            this.dimension.TabIndex = 1;
+            this.dimension.Text = "20x20x2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.start);
+            this.panel1.Controls.Add(this.dimension);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 350);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // start
+            // 
+            this.start.Dock = System.Windows.Forms.DockStyle.Right;
+            this.start.Location = new System.Drawing.Point(581, 0);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(219, 100);
+            this.start.TabIndex = 2;
+            this.start.Text = "Go";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.content);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox content;
+        private System.Windows.Forms.TextBox dimension;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ otp/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace otp
+{
+    public struct Vector2
+    {
+        public int x;
+        public int y;
+    }
+    public struct MOUSEINFO
+    {
+        public Vector2 pos;
+        public uint data;
+        public uint flags;
+        public uint time;
+        public IntPtr extra;
+    }
+
+    public partial class Form1 : Form
+    {
+        uint pow(uint a, uint b)
+        {
+            uint ret = 1;
+
+            for (int i = 0; i < b; i++)
+            {
+                ret *= a;
+            }
+
+            return ret;
+        }
+
+        bool generating = false;
+        bool halt = false;
+
+        uint[] dim = { 0, 0, 0 };
+        List<uint> vals = new List<uint>();
+
+        public delegate IntPtr HookCallbackDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+
+        const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        const int MOUSEEVENTF_LEFTUP = 0x04;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        const int MOUSEEVENTF_RIGHTUP = 0x10;
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, HookCallbackDelegate lpfn, IntPtr wParam, uint lParam);
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        const int WH_KEYBOARD_LL = 13;
+        const int WH_MOUSE_LL = 14;
+        const int WM_KEYDOWN = 0x0100;
+        const int WM_KEYUP = 0x0101;
+        const int WM_ALTKEYDOWN = 0x0104;
+        const int WM_ALTKEYUP = 0x0105;
+        const int WM_MOUSEMOVE = 0x0200;
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+        const int WM_MOUSEWHEEL = 0x020A;
+        const int WM_RBUTTONDOWN = 0x0204;
+        const int WM_RBUTTONUP = 0x0205;
+        const int WM_LBUTTONDBLCLK = 0x0203;
+        const int WM_MBUTTONDOWN = 0x0207;
+        const int WM_MBUTTONUP = 0x020;
+
+        HookCallbackDelegate mhcDelegate;
+        IntPtr whllmousehook;
+
+        IntPtr mhandler(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (wParam == (IntPtr)WM_MOUSEMOVE && !halt)
+            {
+                MOUSEINFO info = (MOUSEINFO)Marshal.PtrToStructure(lParam, typeof(MOUSEINFO));
+
+                HashAlgorithm algo = MD5.Create();
+                byte[] data = algo.ComputeHash(Encoding.UTF8.GetBytes(
+                    info.pos.x.ToString() + info.pos.y.ToString() + DateTime.Now.Ticks.ToString()
+                ));
+
+                uint val = BitConverter.ToUInt32(data, 0) % pow(10, dim[2]);
+                vals.Add(val);
+                start.Text = "Move your mouse\r\n" + (vals.Count * 100 / (dim[0] * dim[1])) + "%";
+
+                if (vals.Count >= dim[0] * dim[1])
+                {
+                    halt = true;
+                    endgen();
+                    endListening();
+                }
+            }
+
+            return CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        public void startListening()
+        {
+            mhcDelegate = mhandler;
+
+            string mainModuleName = Process.GetCurrentProcess().MainModule.ModuleName;
+            whllmousehook = SetWindowsHookEx(WH_MOUSE_LL, mhcDelegate, GetModuleHandle(mainModuleName), 0);
+        }
+        public void endListening()
+        {
+            UnhookWindowsHookEx(whllmousehook);
+            mhcDelegate = null;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            if (generating)
+                return;
+            generating = true;
+
+            start.Text = "Move your mouse";
+
+            dim = dimension.Text.Split('x').Select((x) => uint.Parse(x)).ToArray();
+
+            startListening();
+        }
+
+        private void endgen()
+        {
+            start.Text = "Go";
+
+            content.Text = "";
+            for (int i = 0; i < dim[1]; i++)
+            {
+                for (int j = 0; j < dim[0]; j++)
+                {
+                    content.Text += vals[i * Convert.ToInt32(dim[0]) + j].ToString("D" + dim[2].ToString());
+
+                    if (j != dim[0] - 1)
+                        content.Text += " ";
+                }
+
+                if (i != dim[1] - 1)
+                    content.Text += "\r\n";
+            }
+
+            vals.Clear();
+
+            generating = false;
+            halt = false;
+        }
+    }
+}
